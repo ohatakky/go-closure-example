5ff3b594ecf7444d1a2626997617f86c33dd54fa
ohatakky <ohatakky@gmail.com>
2019-03-23 17:31:38 +0900

    closure

--- main.go
+++ main.go
@@ -2,7 +2,20 @@ package main
 
 import "fmt"
 
+func print_x_closure() func() {
+	x := 0
+	return func() {
+		x = x + 1
+		fmt.Println(x)
+	}
+}
+
 func main() {
+	pxc := print_x_closure()
+	pxc()
+	pxc()
+	pxc()
+
 	var x int
 	defer func() {
 		var x int
